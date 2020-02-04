2d28c8bad775c0d37a21ddf9a5b334ff806b08bf
Luca Morandini <lmorandini@ieee.org>
2020-02-04 15:07:12 +1100

    [AUR-6505] Upgrade QGIS SDMX Plugin from QGIS2 to QGIS3 - Simplified layout (got rid of WFS request text box) - Refactored the GetFeature request

--- sdmx_dialog.py
+++ sdmx_dialog.py
@@ -36,6 +36,7 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'ui/sdmx_dialog_base.ui'))
 PLUGIN_NAME = "SDMXPlugin"
 
+
 class SDMXPluginDialog(QDialog, FORM_CLASS):
 
     def __init__(self, parent=None):
@@ -45,116 +46,113 @@ class SDMXPluginDialog(QDialog, FORM_CLASS):
         self.setupUi(self)
         # TODO: hard-coded for the time being
         self.activeWfsConn = WFSConnection("", "", "", PLUGIN_NAME)
-        
+
         # Load connection data, if saved in the project
         self.loadSettings()
-        
-        self.activeCube= None
-        self.activeDims= set()
-        self.activeMembers= dict()
-        
+
+        self.activeCube = None
+        self.activeDims = set()
+        self.activeMembers = dict()
+
     def cubeItemSelected(self, item, column):
         for i in range(self.treeCubes.invisibleRootItem().childCount()):
-          itemI = self.treeCubes.invisibleRootItem().child(i)
-          itemI.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
-          itemI.setExpanded(False)
-          
+            itemI = self.treeCubes.invisibleRootItem().child(i)
+            itemI.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
+            itemI.setExpanded(False)
+
         item.setIcon(0, self.style().standardIcon(QStyle.SP_DialogApplyButton))
         item.setExpanded(True)
-        self.activeCube=item.data(0, 0) 
+        self.activeCube = item.data(0, 0)
 
         self.fillDimensions(item)
-        self.activeDims= set()
-        self.activeMembers= dict()
+        self.activeDims = set()
+        self.activeMembers = dict()
 
     def dimItemSelected(self, item, column):
-        if item.data(0,0 ).__class__.__name__ == "Dimension":
-          if item.childCount() == 0:
-            self.fillMembers(item)
+        if item.data(0, 0).__class__.__name__ == "Dimension":
+            if item.childCount() == 0:
+                self.fillMembers(item)
 
-          if item.isExpanded():
-            item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
-            item.setExpanded(False)
-            self.activeDims.remove(item.data(0, 0))
-          else:
-            item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowUp))
-            item.setExpanded(True)
-            self.activeDims.add(item.data(0, 0))
+            if item.isExpanded():
+                item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
+                item.setExpanded(False)
+                self.activeDims.remove(item.data(0, 0))
+            else:
+                item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowUp))
+                item.setExpanded(True)
+                self.activeDims.add(item.data(0, 0))
         else:
-          if item.isExpanded():
-            item.setIcon(0, self.style().standardIcon(QStyle.SP_CustomBase))
-            item.setExpanded(False)
-            if item.data(0, 0).dim.name in list(self.activeMembers.keys()):
-              self.activeMembers[item.data(0, 0).dim.name].remove(item.data(0, 0))
-            if len(self.activeMembers[item.data(0, 0).dim.name]) == 0:
-              del self.activeMembers[item.data(0, 0).dim.name]
-          else:
-            item.setIcon(0, self.style().standardIcon(QStyle.SP_DialogApplyButton))
-            item.setExpanded(True)
-            if item.data(0, 0).dim.name not in list(self.activeMembers.keys()):
-              self.activeMembers[item.data(0, 0).dim.name]= set()
-            self.activeMembers[item.data(0, 0).dim.name].add(item.data(0, 0))
+            if item.isExpanded():
+                item.setIcon(0, self.style().standardIcon(QStyle.SP_CustomBase))
+                item.setExpanded(False)
+                if item.data(0, 0).dim.name in list(self.activeMembers.keys()):
+                    self.activeMembers[item.data(0, 0).dim.name].remove(item.data(0, 0))
+                if len(self.activeMembers[item.data(0, 0).dim.name]) == 0:
+                    del self.activeMembers[item.data(0, 0).dim.name]
+            else:
+                item.setIcon(0, self.style().standardIcon(QStyle.SP_DialogApplyButton))
+                item.setExpanded(True)
+                if item.data(0, 0).dim.name not in list(self.activeMembers.keys()):
+                    self.activeMembers[item.data(0, 0).dim.name] = set()
+                self.activeMembers[item.data(0, 0).dim.name].add(item.data(0, 0))
+
+        self.exprShown()
 
     def connect(self):
         self.treeCubes.clear()
-        self.activeWfsConn= WFSConnection(self.wfsUrlInput.text(), 
-           self.usernameInput.text(), self.passwordInput.text(), PLUGIN_NAME)
+        self.activeWfsConn = WFSConnection(self.wfsUrlInput.text(),
+                                           self.usernameInput.text(), self.passwordInput.text(), PLUGIN_NAME)
 
         self.activeWfsConn.connect()
         for cube in self.activeWfsConn.getCubes():
-          item = QTreeWidgetItem(self.treeCubes)
-          item.setText(1, cube.name)
-          item.setData(0, 0, cube)
-          self.treeCubes.insertTopLevelItem(0, item)
+            item = QTreeWidgetItem(self.treeCubes)
+            item.setText(1, cube.name)
+            item.setData(0, 0, cube)
+            self.treeCubes.insertTopLevelItem(0, item)
 
     def fillDimensions(self, cubeItem):
         cube = cubeItem.data(0, 0)
 
         self.treeDimensions.clear()
         for dim in self.activeWfsConn.getCubeDimensions(cube):
-          item = QTreeWidgetItem(self.treeDimensions)
-          item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
-          item.setText(1, dim.description)
-          item.setText(2, dim.name)
-          item.setData(0, 0, dim)
-          self.treeDimensions.insertTopLevelItem(0, item)
+            item = QTreeWidgetItem(self.treeDimensions)
+            item.setIcon(0, self.style().standardIcon(QStyle.SP_ArrowDown))
+            item.setText(1, dim.description)
+            item.setText(2, dim.name)
+            item.setData(0, 0, dim)
+            self.treeDimensions.insertTopLevelItem(0, item)
 
     def fillMembers(self, subTree):
         dim = subTree.data(0, 0)
         for m in self.activeWfsConn.getDimensionMembers(dim).members:
-          item = QTreeWidgetItem(subTree)
-          item.setText(1, m.value)
-          item.setText(2, m.code)
-          item.setData(0, 0, m)
-          subTree.addChild(item)
+            item = QTreeWidgetItem(subTree)
+            item.setText(1, m.value)
+            item.setText(2, m.code)
+            item.setData(0, 0, m)
+            subTree.addChild(item)
 
     def selectMember(self, member):
         value = member.data(0, 0)
 
-    def exprShown(self, tabNumber):
-        if tabNumber != 2:
-          return
+    def exprShown(self):
         if self.activeCube != None:
-          exprDims= list()
-          for dim in self.activeDims:
-            exprMembers= list()
-            if dim.name in list(self.activeMembers.keys()):
-              for m in self.activeMembers[dim.name]:
-                exprMembers.append("'" + m.code + "'")
-              exprDims.append(dim.name + " in (" + ",".join(exprMembers) + ")" )
-          cqlExpr= " and ".join(exprDims) 
-          self.wfsExpr.setText(unquote(self.activeWfsConn.getFeatureURL(self.activeCube.featureType, cqlExpr)))
-          self.sqlExpr.setText(cqlExpr)
+            exprDims = list()
+            for dim in self.activeDims:
+                exprMembers = list()
+                if dim.name in list(self.activeMembers.keys()):
+                    for m in self.activeMembers[dim.name]:
+                        exprMembers.append("'" + m.code + "'")
+                    exprDims.append(dim.name + " in (" + ",".join(exprMembers) + ")")
+            cqlExpr = " and ".join(exprDims)
+
+            self.cqlExpr.setText(unquote(cqlExpr))
 
     def executeWFSRequest(self):
-        msg = "About to ecxcute WFS request %s" % self.wfsExpr.toPlainText()
-        QgsMessageLog.logMessage(msg, 'Info')
-        response = requests.get(self.wfsExpr.toPlainText(), auth=self.activeWfsConn.auth)
-        response.raise_for_status()
-        tmpCsv= tempfile.NamedTemporaryFile(mode = 'w', suffix = '.csv', prefix = 'sdmx-', delete=False)
+        response = self.activeWfsConn.executeGetFeatureRequest(self.activeCube.featureType, self.cqlExpr.toPlainText())
+        tmpCsv = tempfile.NamedTemporaryFile(mode='w', suffix='.csv', prefix='sdmx-', delete=False)
         tmpCsv.write(response.text)
         tmpCsv.close()
-        table = QgsVectorLayer('file://' + tmpCsv.name, 'SDMX:: ' + self.sqlExpr.toPlainText(), 'delimitedtext')
+        table = QgsVectorLayer('file://' + tmpCsv.name, 'SDMX:: ' + self.cqlExpr.toPlainText(), 'delimitedtext')
         QgsProject.instance().addMapLayer(table)
 
     # FIXME: this would be handy to load the WFS URL from QGIS settings, excepts it does not work in QGSI3
@@ -166,7 +164,7 @@ class SDMXPluginDialog(QDialog, FORM_CLASS):
 
     # FIXME: this would be handy to save the current WFS URL to QGIS settings, excepts it does not work in QGSI3
     def saveSettings(self):
-        self.activeWfsConn= WFSConnection(self.wfsUrlInput.text(), 
-           self.usernameInput.text(), self.passwordInput.text(), PLUGIN_NAME)
-        self.connections= QgsProject.instance().writeEntry (PLUGIN_NAME, "connection", 
-           self.activeWfsConn.encode())
+        self.activeWfsConn = WFSConnection(self.wfsUrlInput.text(),
+                                           self.usernameInput.text(), self.passwordInput.text(), PLUGIN_NAME)
+        self.connections = QgsProject.instance().writeEntry(PLUGIN_NAME, "connection",
+                                                            self.activeWfsConn.encode())
--- wfs_connection.py
+++ wfs_connection.py
@@ -7,8 +7,11 @@ from qgis.gui import QgsMessageBar
 from qgis.utils import iface
 from libpasteurize.fixes import feature_base
 from .cube import Cube, Member, Members, Dimension
+from urllib.parse import unquote
 
 CONNECTION_SEP = "____"
+WFS_VERSION = "1.1.0"
+
 
 class WFSConnection(object):
 
@@ -28,81 +31,76 @@ class WFSConnection(object):
 
         # Connects to the WFS server to return feature types
         if self.username:
-          self.auth = (self.username, self.password)
+            self.auth = (self.username, self.password)
         else:
-          self.auth = None
+            self.auth = None
 
     def encode(self):
-      """Encode the connection parameters into a single String and returns it"""
-      return base64.b64encode(CONNECTION_SEP.join((self.url, self.username, self.password)).encode('utf-8')).decode('utf-8')
+        """Encode the connection parameters into a single String and returns it"""
+        return base64.b64encode(CONNECTION_SEP.join((self.url, self.username, self.password)).encode('utf-8')).decode(
+            'utf-8')
 
     def decode(self, connString):
-      """Decode the connection parameters from a string and update the connection with them"""
-      connList= base64.b64decode(connString.encode('utf-8')).decode('utf-8').split(CONNECTION_SEP)
-                
-      self.url= connList[0] if len(connList) > 0 else None 
-      self.username= connList[1] if len(connList) > 1 else None
-      self.password= connList[2] if len(connList) > 2 else None
-      
+        """Decode the connection parameters from a string and update the connection with them"""
+        connList = base64.b64decode(connString.encode('utf-8')).decode('utf-8').split(CONNECTION_SEP)
+
+        self.url = connList[0] if len(connList) > 0 else None
+        self.username = connList[1] if len(connList) > 1 else None
+        self.password = connList[2] if len(connList) > 2 else None
+
     def connect(self):
         try:
-          resp = requests.get(self.url, auth=self.auth,
-                            params={"request":"GetCapabilities", "version":"1.1.0", "service":"wfs"})
+            resp = requests.get(self.url, auth=self.auth,
+                                params={"request": "GetCapabilities", "version": WFS_VERSION, "service": "wfs"})
         except requests.exceptions.RequestException as e:
-          msg= "Error connecting to WFS server %s" % str(e)
-          QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-          iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-          return
+            msg = "Error connecting to WFS server %s" % str(e)
+            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+            return
 
         try:
-          self.capabilities = lxml.etree.XML(resp.content)
-          self.featureTypes = self.capabilities.findall(
+            self.capabilities = lxml.etree.XML(resp.content)
+            self.featureTypes = self.capabilities.findall(
                 ".//{http://www.opengis.net/wfs}FeatureType/{http://www.opengis.net/wfs}Name")
         except lxml.etree.XMLSyntaxError as e:
-          msg= "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
-          QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-          iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-          return
+            msg = "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
+            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+            return
 
         # Extracts cube feature types
-        self.cubes = [Cube(m2.group(1), m2.group(2), m2.group(0)) for m2 in [m for m in [self.cubeRe.search(e.text) for e in self.featureTypes] if m is not None]]
+        self.cubes = [Cube(m2.group(1), m2.group(2), m2.group(0)) for m2 in
+                      [m for m in [self.cubeRe.search(e.text) for e in self.featureTypes] if m is not None]]
 
         # Extract cube dimensions
         for cube in self.cubes:
-          try:
-            resp = requests.get(self.url, auth=self.auth,
-                params={"request":"GetFeature", "version":"1.1.0", "service":"wfs", 
-                        "typeName":cube.dimFeatureType, "cql_filter":"CODE='ALL'"})
-          except requests.exceptions.RequestException as e:
-            msg= "Error connecting to WFS server %s" % str(e)
-            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-            return
-            
-          try:
-            features = lxml.etree.XML(resp.content).find(".//{http://www.opengis.net/gml}featureMembers")
+            try:
+                resp = requests.get(self.url, auth=self.auth,
+                                    params={"request": "GetFeature", "version": WFS_VERSION, "service": "wfs",
+                                            "typeName": cube.dimFeatureType, "cql_filter": "CODE='ALL'"})
+            except requests.exceptions.RequestException as e:
+                msg = "Error connecting to WFS server %s" % str(e)
+                QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+                iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+                return
 
-          except lxml.etree.XMLSyntaxError as e:
-            msg= "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
-            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-            return
+            try:
+                features = lxml.etree.XML(resp.content).find(".//{http://www.opengis.net/gml}featureMembers")
 
-          if features is None:
-            msg="No feature types were returned"
-            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-            return
+            except lxml.etree.XMLSyntaxError as e:
+                msg = "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
+                QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+                iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+                return
 
-          self.dimensions[cube.featureType]= [Dimension(feat[0].text, feat[1].text, cube.name, cube.dimFeatureType) for feat in features]
+            if features is None:
+                msg = "No feature types were returned"
+                QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+                iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+                return
 
-    def getFeatureURL(self, featureType, cqlExpr):
-        """ Returns an URL to get data (in CSV format) from the given feature type using the given CQL expression"""
-        req = requests.Request("GET", self.url, params={"request":"GetFeature", "version":"1.1.0",
-                                                      "service":"wfs", "outputFormat":"csv", "typeName":featureType, "cql_filter": cqlExpr})
-        proxies = dict()
-        proxies[self.url.split("://")[0]] = self.url.split("//")[1]
-        return requests.adapters.HTTPAdapter().request_url(requests.Session().prepare_request(req), proxies)
+            self.dimensions[cube.featureType] = [Dimension(feat[0].text, feat[1].text, cube.name, cube.dimFeatureType)
+                                                 for feat in features]
 
     def getCubes(self):
         # Returns a list of SDMX cubes
@@ -114,26 +112,49 @@ class WFSConnection(object):
     def getDimensionMembers(self, dim):
         # Returns a list of SDMX members for a given dimension
         try:
-          resp = requests.get(self.url, auth=self.auth,
-              params={"request":"GetFeature", "version":"1.1.0", "service":"wfs", 
-                        "typeName":dim.featureType, "cql_filter":"CODE='" + dim.name + "'"})
+            resp = requests.get(self.url, auth=self.auth,
+                                params={"request": "GetFeature", "version": WFS_VERSION, "service": "wfs",
+                                        "typeName": dim.featureType, "cql_filter": "CODE='" + dim.name + "'"})
         except requests.exceptions.RequestException as e:
-          msg= "Error connecting to WFS server %s" % str(e)
-          QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-          iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-          return
-            
+            msg = "Error connecting to WFS server %s" % str(e)
+            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+            return
+
         try:
-          features = lxml.etree.XML(resp.content).find(".//{http://www.opengis.net/gml}featureMembers")
+            features = lxml.etree.XML(resp.content).find(".//{http://www.opengis.net/gml}featureMembers")
 
         except lxml.etree.XMLSyntaxError as e:
-          msg= "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
-          QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
-          iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
-          return
-        
+            msg = "Error retrieving metadata from WFS server %s, please check URL and authentication" % str(e)
+            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+            return
+
         m = Members(dim)
-        m.members= [Member(dim, feat[0].text, feat[1].text) for feat in features]   
+        m.members = [Member(dim, feat[0].text, feat[1].text) for feat in features]
         m.members.sort(key=lambda m: m.value)
         return m
 
+    def getFeatureURL(self, featureType, cqlExpr):
+        """ Returns an URL to get data (in CSV format) from the given feature type using the given CQL expression"""
+        req = requests.Request("GET", self.url, params={"request": "GetFeature", "version": WFS_VERSION,
+                                                        "service": "wfs", "outputFormat": "csv",
+                                                        "typeName": featureType, "cql_filter": cqlExpr})
+        proxies = dict()
+        proxies[self.url.split("://")[0]] = self.url.split("//")[1]
+        return requests.adapters.HTTPAdapter().request_url(requests.Session().prepare_request(req), proxies)
+
+    def executeGetFeatureRequest(self, featureType, cqlExpr):
+        try:
+            resp = requests.get(self.url, auth=self.auth,
+                                params={"request": "GetFeature", "version": WFS_VERSION, "service": "wfs",
+                                        "outputFormat": "csv",
+                                        "typeName": featureType, "cql_filter": cqlExpr})
+
+        except requests.exceptions.RequestException as e:
+            msg = "Error connecting to WFS server %s" % str(e)
+            QgsMessageLog.logMessage(msg, self.loggerName, Qgis.Critical)
+            iface.messageBar().pushMessage("Error", msg, level=Qgis.Critical)
+            return
+
+        return resp
